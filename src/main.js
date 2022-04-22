0ebc1f696a8fcd05eca748de31f896688c685b26
Cecil Choi <ceec@ucsc.edu>
2022-04-22 14:11:50 -0700

    crowd bg scroll

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// CECIL TESTING
-
 let config = {
     type: Phaser.AUTO,
     width: 650,
@@ -13,6 +11,9 @@ let game = new Phaser.Game(config);
 
 // reserve keyboard vars
 let keyR, keyM, keyLEFT, keyRIGHT, keyUP, keyA, keyD, keyW;
+
+// set scroll speed
+let scrollSpeed = 1.5;
 
 let borderUISize = game.config.height / 25; 
 let borderPadding = borderUISize / 3;
